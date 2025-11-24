5d6560a381fb16c4c6ccdb2ba4fb39c32054d895
Pinja Parviainen <pinja.t.parviainen@gmail.com>
2025-11-24 21:36:35 +0200

    valmis

--- mod_04/m04t2/t2.js
+++ mod_04/m04t2/t2.js
@@ -8,20 +8,11 @@ form.addEventListener('submit', async function asynchronousFunction(evt)
   evt.preventDefault();
   const formData = new FormData(tvForm);
   const queryString = new URLSearchParams(formData).toString();
-  //const value_from_input =
-  //const value_from_input = document.querySelector('input[name=q]').value;
   try {
     const response = await fetch(`${apiURL}/search/shows?q=${queryString}`);
-    //https://api.tvmaze.com/search/shows?q=${value_from_input}
     const jsonData = await response.json();
-    if (!jsonData) {
-      console.log('Tv-ohjelmaa ei löydy');
-    }
-    else {
-      console.log(jsonData);
-    }
-    //console.log(jsonData);
-  } catch (error) {
+    console.log(jsonData);
+    } catch (error) {
     console.error(error.message);
   }
 });
